3eba76084d46c8683acf085efd0805c8de49aad8
Muhammad Usman <muhammad.usman@aalto.fi>
2013-09-16 12:20:30 +0300

    Main controller modified as per the variables

--- app/js/controllers/MainController.js
+++ app/js/controllers/MainController.js
@@ -1,9 +1,34 @@
 myCalendar.controller('MainController', ['$scope', '$location', function($scope, $location) {
+
+	$scope.homeTab = "Home";
+
+	$scope.calendarTab = "Calendar";
+
+	$scope.mapTab = "Map";
+
+	$scope.searchTab = "Search";
+
+	$scope.logoutTab = "Logout";
+
+	$scope.addEvent = "Add Event";
+
+	$scope.editEvent = "Edit Event";
+
+	$scope.cancelBtn = "Cancel";
+
+	$scope.sendBtn = "Send";
+
+	$scope.deleteBtn = "Delete";
+
+	$scope.openHome = function(route) {
+    	$location.path('/home');
+    };
+
 	$scope.openCalendar = function(route) {
         $location.path('/calendar');
     };
 
-    $scope.logoutButton = function() {
+    $scope.logoutButton = function(route) {
         $location.path('/logout');
     };
 }]);
