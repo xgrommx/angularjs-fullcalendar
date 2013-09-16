60142bbb09a075c5a837ee1f69916e9751dba2da
Muhammad Usman <muhammad.usman@aalto.fi>
2013-09-16 12:20:03 +0300

    Logout controller modified as per the variables

--- app/js/controllers/LogoutController.js
+++ app/js/controllers/LogoutController.js
@@ -1,4 +1,6 @@
 myCalendar.controller('LogoutController', ['$scope', 'AuthenticationService', function($scope, AuthenticationService) {
+	$scope.loggedoutTitle = "Logged Out";
+	$scope.loginBack = "Log in back";
 	$scope.logout = function() {
 		AuthenticationService.logout();
 	};
